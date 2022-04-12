0cf57d18871b1fc83469973f82646ceb468bf204
sso-hyeon <ashash1994@naver.com>
2022-04-12 16:06:36 +0900

    git 배포 test

--- src/App.js
+++ src/App.js
@@ -14,6 +14,9 @@ function App() {
     useEffect(() => {
         console.log("I run when 'counter' changes.");
     }, [counter]);
+    useEffect(() => {
+        console.log("I run when keyword & counter changes.");
+    }, [keyword, counter]);
     return (
         <div>
             <input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
