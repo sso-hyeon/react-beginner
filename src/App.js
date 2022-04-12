64d620860f30036ed810c2b3c63ad290f154089f
sso-hyeon <ashash1994@naver.com>
2022-04-12 14:20:01 +0900

    useEffect study

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import Button from "./Button";
+import styles from "./App.module.css";
+import { useState, useEffect } from "react";
+
 function App() {
+    const [counter, setValue] = useState(0);
+    const [keyword, setKeyword] = useState("");
+    const onClick = () => setValue(prev => prev + 1);
+    const onChange = event => setKeyword(event.target.value);
+    useEffect(() => {
+        console.log("I run only once.");
+    }, []);
+    useEffect(() => {
+        console.log("I run when 'keyword' changes.");
+    }, [keyword]);
+    useEffect(() => {
+        console.log("I run when 'counter' changes.");
+    }, [counter]);
     return (
         <div>
-            <h1>Welcome Back!!!</h1>
+            <input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
+            <h1>{counter}</h1>
+            <button onClick={onClick}>click me</button>
+            {/* <Button text={"Continue"} /> */}
         </div>
     );
 }
