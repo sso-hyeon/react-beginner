60f77de24fc9b9b8d63686ce5dc12b02692d99f6
sso-hyeon <ashash1994@naver.com>
2022-04-15 17:26:23 +0900

    react toDoList study

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ function App() {
         setToDos(currentArray => [toDo, ...currentArray]);
         setToDo("");
     };
-    console.log(toDos);
     return (
         <div>
             <h1>My To Dos ({toDos.length})</h1>
@@ -20,6 +19,12 @@ function App() {
                 <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do..."></input>
                 <button>Add To Do</button>
             </form>
+            <hr />
+            <ul>
+                {toDos.map((item, index) => (
+                    <li key={index}>{item}</li>
+                ))}
+            </ul>
         </div>
     );
 }
