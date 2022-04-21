22cdd8abefa3a289d80c35f15513117aba0798c7
sso-hyeon <ashash1994@naver.com>
2022-04-21 17:48:26 +0900

    coin tracker study

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import Coin from "./Coin";
 
 const root = document.getElementById("root");
 
 ReactDOM.createRoot(root).render(
     <React.StrictMode>
-        <App />
+        {/* <App /> */}
+        <Coin />
     </React.StrictMode>
 );
