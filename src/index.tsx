9aed848cf07990dd4e23e9160e217fea631f6396
sso-hyeon <ashash1994@naver.com>
2022-05-17 15:18:45 +0900

    react-hook-form study

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { RecoilRoot } from "recoil";
+import { ThemeProvider } from "styled-components";
+import App from "./App";
+import { darkTheme } from "./theme";
+
+ReactDOM.render(
+    <>
+        <RecoilRoot>
+            <ThemeProvider theme={darkTheme}>
+                <App />
+            </ThemeProvider>
+        </RecoilRoot>
+    </>,
+    document.getElementById("root")
+);
